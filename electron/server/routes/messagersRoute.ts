3e2abbfb6989ec0548985fb50522d1aea530c807
FQZ <617564112@qq.com>
2024-07-18 19:27:03 +0800

    1

--- electron/server/routes/messagersRoute.ts
+++ electron/server/routes/messagersRoute.ts
@@ -2,8 +2,6 @@ import express from "express";
 import { register, emit } from "../controllers/messagersControllers";
 const router = express.Router();
 
-const messageListeners = {};
-
 router.get("/register", register);
 
 router.get("/emit", emit);
